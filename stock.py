cb8308c35e3e9446b6381b427bc18c725947060f
Kelson-G <kelson.gneiting@gmail.com>
2025-01-13 02:42:29 -0700

    created a for loop to show how we can access files data

--- stock.py
+++ stock.py
@@ -5,18 +5,28 @@ import pandas as pd
 stock_managing_data = pd.read_csv("Dataset/symbols_valid_meta.csv")
 
 # Removing columns for easier organization (I may change which columns later but this should work for now)
-stock_managing_data = stock_managing_data.drop(columns=['Nasdaq Traded','Listing Exchange','Market Category','Round Lot Size','Test Issue','Financial Status','CQS Symbol','NASDAQ Symbol','NextShares'])
+stock_managing_data = stock_managing_data.drop(columns=['Nasdaq Traded','Listing Exchange','Market Category','Round Lot Size','Test Issue','Financial Status','CQS Symbol','Symbol','NextShares'])
 
 # Seperates the ETF column into 2 columns. ETF_N and ETF_Y 
 stock_managing_data = pd.get_dummies(stock_managing_data, columns=["ETF"])
 
 # The data looks like this: print(stock_managing_data.head(3))
-#   Symbol                            Security Name  ETF_N  ETF_Y
-# 0      A  Agilent Technologies, Inc. Common Stock   True  False
-# 1     AA          Alcoa Corporation Common Stock    True  False
-# 2   AAAU             Perth Mint Physical Gold ETF  False   True
+#                              Security Name NASDAQ Symbol  ETF_N  ETF_Y
+# 0  Agilent Technologies, Inc. Common Stock             A   True  False
+# 1          Alcoa Corporation Common Stock             AA   True  False
+# 2             Perth Mint Physical Gold ETF          AAAU  False   True
 
-# for symbol in stock_managing_data["Symbol"]:
-#     if stock_managing_data
-#     data = pd.read_csv(f"Dataset/stocks/{symbol}.csv")
-#     print(data.head(1))
+# Using the list of names it reads the data on each csv file using the symbol 
+for row in range(len(stock_managing_data)):
+    symbol = stock_managing_data["NASDAQ Symbol"][row]
+
+    # PRN.csv is not a valid name. We will need a better work around later. I renamed the file to include a 1. Its hardcoded...
+    if symbol == "PRN":
+        symbol = "PRN1"
+
+    if stock_managing_data["ETF_N"][row] == True:
+        data = pd.read_csv(f"Dataset/stocks/{symbol}.csv")
+    else:
+        data = pd.read_csv(f"Dataset/etfs/{symbol}.csv")
+
+print("It worked")
