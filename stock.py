73f7f954bfc843d53e17bb3406abe48bd78a5f3c
Kelson-G <kelson.gneiting@gmail.com>
2025-01-13 01:41:23 -0700

    Used pandas get_dummies to organize ETFs and make it so we can use that information. Started a for loop as well but need the data to continue

--- stock.py
+++ stock.py
@@ -4,6 +4,19 @@ import pandas as pd
 # Reading the Data
 stock_managing_data = pd.read_csv("Dataset/symbols_valid_meta.csv")
 
-# Removing columns for easier organization
-stock_managing_data = stock_managing_data.drop(columns=['Nasdaq Traded','Listing Exchange','Market Category','ETF','Round Lot Size','Test Issue','Financial Status','CQS Symbol','NASDAQ Symbol','NextShares'])
-# print(stock_managing_data.head())
+# Removing columns for easier organization (I may change which columns later but this should work for now)
+stock_managing_data = stock_managing_data.drop(columns=['Nasdaq Traded','Listing Exchange','Market Category','Round Lot Size','Test Issue','Financial Status','CQS Symbol','NASDAQ Symbol','NextShares'])
+
+# Seperates the ETF column into 2 columns. ETF_N and ETF_Y 
+stock_managing_data = pd.get_dummies(stock_managing_data, columns=["ETF"])
+
+# The data looks like this: print(stock_managing_data.head(3))
+#   Symbol                            Security Name  ETF_N  ETF_Y
+# 0      A  Agilent Technologies, Inc. Common Stock   True  False
+# 1     AA          Alcoa Corporation Common Stock    True  False
+# 2   AAAU             Perth Mint Physical Gold ETF  False   True
+
+# for symbol in stock_managing_data["Symbol"]:
+#     if stock_managing_data
+#     data = pd.read_csv(f"Dataset/stocks/{symbol}.csv")
+#     print(data.head(1))
