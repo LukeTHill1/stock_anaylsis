4c85dc72bf6abffcabd723ea32aeb98ec7272c79
Kelson-G <kelson.gneiting@gmail.com>
2025-01-13 02:54:56 -0700

    Showed the Data.head so we have an idea

--- stock.py
+++ stock.py
@@ -28,5 +28,11 @@ for row in range(len(stock_managing_data)):
         data = pd.read_csv(f"Dataset/stocks/{symbol}.csv")
     else:
         data = pd.read_csv(f"Dataset/etfs/{symbol}.csv")
+    
+# The data looks like this: print(data.head(3))
+#          Date       Open       High        Low      Close  Adj Close    Volume
+# 0  1999-11-18  32.546494  35.765381  28.612303  31.473534  27.068665  62546300
+# 1  1999-11-19  30.713520  30.758226  28.478184  28.880543  24.838577  15234100
+# 2  1999-11-22  29.551144  31.473534  28.657009  31.473534  27.068665   6577800
 
 print("It worked")
