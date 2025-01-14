c34ed2b7a053d7432cbe0c89deb8bcd8916b9707
Kelson-G <kelson.gneiting@gmail.com>
2025-01-14 16:26:55 -0700

    Planned a bit for market trend

--- market_trends.py
+++ market_trends.py
@@ -4,12 +4,17 @@ import pandas as pd
 from sklearn.linear_model import SGDRegressor # I chose this model as it allows for Incremental Learning which will be needed for the multiple files
 from sklearn.model_selection import train_test_split
 
+### MAIN GOAL: Get Model to make predictions
+### FUTURE PLANS: Make a csv file that is structured for a linear regression model using the mean market trend of each day.
+
+
 # Initializing the model 
 model = SGDRegressor()
 
-# Running tests to learn the model. Will explain tomorrow. Working with rows are weird
+# Running tests to learn the model. Will explain when I actually understand. Working with rows are weird
 test_data = pd.read_csv("Dataset/stocks/A.csv")
 test_data = test_data.drop(columns=['Date'], axis=1)
+# Not doing what I would like. With a closer look at the data its saying the next day, not past day.
 test_data['Previous_Close'] = test_data['Close'].shift(1)
 
 test_data = test_data.dropna()
